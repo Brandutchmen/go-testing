49647a807b638db6b30c6ab4301d5fd529326175
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2023-08-20 01:16:32 -0500

    Added server websockets goroutine

--- web/main.go
+++ web/main.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gorilla/websocket"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-  ReadBufferSize: 1024,
-  WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 func main() {
-  http.HandleFunc("/echo", echo_handler)
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+    go pushMessages(conn)
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+
+	fmt.Printf("Server starting at port 8080\n")
+	http.ListenAndServe(":8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  http.ServeFile(w, r, "index.html")
-}
-
-func echo_handler(w http.ResponseWriter, r *http.Request) {
-  conn, _ := upgrader.Upgrade(w, r, nil)
-
-  for {
-    msgType, msg, err := conn.ReadMessage()
-    if err != nil {
-      return
-    }
-
-    fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-    if err = conn.WriteMessage(msgType, msg); err != nil {
-      return
-    }
-  }
+func pushMessages(conn *websocket.Conn) {
+	for {
+    time.Sleep(5 * time.Second)
+		message := []byte("Server push: " + time.Now().Format(time.RFC3339))
+		conn.WriteMessage(websocket.TextMessage, message)
+	}
 }
