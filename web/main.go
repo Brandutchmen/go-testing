1489a9df0e2579e4c70917a40027f4d2893ba6c2
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2023-08-20 01:38:28 -0500

    format

--- web/main.go
+++ web/main.go
@@ -16,7 +16,7 @@ var upgrader = websocket.Upgrader{
 func main() {
 	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-    go pushMessages(conn)
+		go pushMessages(conn)
 
 		for {
 			// Read message from browser
@@ -39,14 +39,13 @@ func main() {
 		http.ServeFile(w, r, "index.html")
 	})
 
-
 	fmt.Printf("Server starting at port 8080\n")
 	http.ListenAndServe(":8080", nil)
 }
 
 func pushMessages(conn *websocket.Conn) {
 	for {
-    time.Sleep(5 * time.Second)
+		time.Sleep(5 * time.Second)
 		message := []byte("Server push: " + time.Now().Format(time.RFC3339))
 		conn.WriteMessage(websocket.TextMessage, message)
 	}
