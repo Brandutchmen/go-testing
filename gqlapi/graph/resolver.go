b3b8ee2730563f58a8cbf2f90f083054e54312bb
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2023-08-25 00:11:31 -0500

    Go GraphQL

--- gqlapi/graph/resolver.go
+++ gqlapi/graph/resolver.go
@@ -1,7 +1,13 @@
+//go:generate go run github.com/99designs/gqlgen generate
 package graph
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
+import (
+  "brandon/gqlapi/graph/model"
+)
 
-type Resolver struct{}
+type Resolver struct{
+  todos []*model.Todo
+}
